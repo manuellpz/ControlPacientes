e3747c2164ba1a708f65557fac6e9214c003117a
manuellpz <54088833+manuellpz@users.noreply.github.com>
2022-04-21 17:19:17 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Login Médico</title>
+   <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+   <header>
+      <h1 class="title">INICIAR SESION</h1>
+   </header>
+   <br>
+   <div id="login_container">
+      
+   </div>
+
+
+
+
+
+
+   <script src="./js/app.js"></script>
+</body>
+</html>
