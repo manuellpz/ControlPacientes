14d5e874f151d457ae7514c8622aa712352a0693
manuellpz <54088833+manuellpz@users.noreply.github.com>
2022-06-05 13:21:56 -0600

    some validations

--- backend/admin.php
+++ backend/admin.php
@@ -6,6 +6,10 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" type="text/css" href="../css/styles.css">
    <title>Administrador</title>
+   <script>
+      if(!sessionStorage.getItem('userdata'))
+         window.location.href = '../'
+   </script>
 </head>
 <body>
    <header>
--- js/admin.js
+++ js/admin.js
@@ -3,11 +3,15 @@ import { select } from '../js/utils.js'
 
 const data = JSON.parse(sessionStorage.getItem('userdata'))
 
+if(data.length > 0)
+   select('#saludo').textContent = `ADMIN: ${data[0].nombres} ${data[0].apellidos}`
+else
+   window.location.href = '../'
+
 select('#logout').addEventListener('click',() => {
    sessionStorage.removeItem('userdata')
    window.location.href = '../'
 })
 
 
-select('#saludo').textContent = `Bienvenido ${data[0].nombres} ${data[0].apellidos}`
 
